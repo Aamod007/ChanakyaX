2a2a8f940e21507b4fd7298f98b0c4ad7ba78341
Aamod Kumar <119789532+Aamod007@users.noreply.github.com>
2025-06-20 09:06:34 +0530

    Update deployCommands.ts

--- src/deploy/deployCommands.ts
+++ src/deploy/deployCommands.ts
@@ -7,15 +7,12 @@ const CLIENT_ID = process.env.DISCORD_LLM_BOT_CLIENT_ID;
 
 const deploy = async () => {
     const commands = [];
-    // Grab all the command files from the commands directory you created earlier
     const foldersPath = path.join(__dirname, '../commands');
     const commandFolders = fs.readdirSync(foldersPath);
     
     for (const folder of commandFolders) {
-        // Grab all the command files from the commands directory you created earlier
         const commandsPath = path.join(foldersPath, folder);
         const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-        // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
         for (const file of commandFiles) {
             const filePath = path.join(commandsPath, file);
             const command = require(filePath);
@@ -27,15 +24,12 @@ const deploy = async () => {
         }
     }
     
-    // Construct and prepare an instance of the REST module
     const rest = new REST().setToken(BOT_TOKEN ?? "");
     
-    // and deploy your commands!
     (async () => {
         try {
             console.log(`Started refreshing ${commands.length} application (/) commands.`);
     
-            // The put method is used to fully refresh all commands in the guild with the current set
             const data: any = await rest.put(
                 Routes.applicationCommands(CLIENT_ID ?? ""),
                 { body: commands }
@@ -43,7 +37,7 @@ const deploy = async () => {
     
             console.log(`Successfully reloaded ${data.length} application (/) commands.`);
         } catch (error) {
-            // And of course, make sure you catch and log any errors!
+            
             console.error(error);
         }
     })();
